152755a2073f41ecf908dcff316924ff1cf8197b
Weslley Fernando Teixeira Chaves <weslleyfernando71609@gmail.com>
2025-05-29 23:06:22 -0300

    feat: datas para os graficos

---
+++ src/pages/models/datas-models.ts
@@ -0,0 +1,4 @@
+export type CustomLabelProps = {
+  name: string;
+  percent: number;
+};
---
+++ src/utils/datas.ts
@@ -0,0 +1,34 @@
+export const lineData = [
+  { name: "Jun", value: 1200 },
+  { name: "Jul", value: 2200 },
+  { name: "Aug", value: 4500 },
+  { name: "Sep", value: 4200 },
+  { name: "Oct", value: 6000 },
+  { name: "Nov", value: 5800 },
+  { name: "Dec", value: 4000 },
+  { name: "Jan", value: 7000 },
+  { name: "Feb", value: 7500 },
+  { name: "Mar", value: 8500 },
+];
+
+export const pieData = [
+  { name: "18-24", value: 30 },
+  { name: "25-34", value: 10 },
+  { name: "35-44", value: 2 },
+  { name: "45+", value: 23 },
+];
+
+export const barData = [
+  { name: "Jun", value: 12 },
+  { name: "Jul", value: 16 },
+  { name: "Aug", value: 18 },
+  { name: "Sep", value: 20 },
+  { name: "Oct", value: 22 },
+  { name: "Nov", value: 19 },
+  { name: "Dec", value: 21 },
+  { name: "Jan", value: 25 },
+  { name: "Feb", value: 27 },
+  { name: "Mar", value: 30 },
+];
+
+export const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
